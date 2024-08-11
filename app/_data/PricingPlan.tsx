4178ecbafc2266a871f6e2d862c1bebec8d40e51
zulfiqarAlibalti <zulfibalti592@gmail.com>
2024-08-11 15:24:22 +0500

    added stripe

--- app/_data/PricingPlan.tsx
+++ app/_data/PricingPlan.tsx
@@ -1,12 +1,12 @@
 export default[
     {
-        link:'',
+        link:'https://buy.stripe.com/test_6oE6oZa0p9dSgakfYY',
         price:7.16,
         priceId:'price_1PkKTdFsj2IrOE8J3xeOARAL',
         duration:'Monthly'
     },
     {
-        link:'',
+        link:'https://buy.stripe.com/test_14kcNna0p4XCaQ0cMN',
         price:50.00,
         priceId:'price_1PkKJwFsj2IrOE8JAboJ5ZcU',
         duration:'Yearly'
